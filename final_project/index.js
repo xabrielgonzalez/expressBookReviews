35da4235f6ea3f49d54c6a96bccf864ab6041891
Xabriel Gonzalez <109489006+xabrielgonzalez@users.noreply.github.com>
2023-02-14 01:17:22 -0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,28 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req,res,next) {
+  
+    if (req.session.authorization) {
+     
+        token = req.session.authorization['accessToken'];
+
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // cache the user object in the request
+                req.user = user;
+                next();
+            }
+         
+            else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
+    // there has not been a log in even in this session
+    else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
  
 const PORT =5000;
